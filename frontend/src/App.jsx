2c497068a03c5681ec532aab862e8c1325f5ca68
priyanshukumarsinha <1ds22cb039@dsce.edu.in>
2024-05-09 06:51:40 +0530

    added a loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setError(false)
+        setLoading(true);
+        const response = await axios.get('/api/products')
+        console.log(response.data);
+        setProducts(response.data)
+        setLoading(false);
+      } catch (error) {
+        setError(true)
+        setLoading(false);
+      }
+    })()
+  }, [])
+
+  if (error) {
+    return <h2>Something Went Wrong !!</h2>
+  }
+
+  if (loading) {
+    return (
+
+      <h2>Loading ...</h2>
+
+    )
+  }
 
   return (
     <>
       <h1>API Handling in React</h1>
+      <h2>Number of Products are : {products.length}</h2>
     </>
   )
 }
